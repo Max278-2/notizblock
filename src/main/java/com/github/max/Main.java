402fc262cbfacebe97802afc7afec19185d0520e
max <max.278.schmidt@gmail.com>
2025-05-24 16:04:03 +0200

    another change to project structure, added empty main view window

--- src/main/java/com/github/max/Main.java
+++ src/main/java/com/github/max/Main.java
@@ -1,7 +1,32 @@
 package com.github.max;
 
-public class Main {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(javafx.stage.Stage primaryStage) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/view/mainView.fxml"));
+            primaryStage.setTitle("Notizblock");
+            primaryStage.setScene(new Scene(root, 300, 200));
+            primaryStage.show();
+        }
+        catch(IOException e)
+        {
+            System.out.println("the root couldn't be initialized because the file couldn't be loaded");
+        }
+       
+        
+
     }
 }
