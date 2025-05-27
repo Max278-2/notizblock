b71c3595dcddb7124af9313e1b29faa9bcd86991
max <max.278.schmidt@gmail.com>
2025-05-27 15:10:54 +0200

    added getters to Note

--- src/main/java/com/github/max/domain/entitys/Note.java
+++ src/main/java/com/github/max/domain/entitys/Note.java
@@ -1,5 +1,34 @@
 package com.github.max.domain.entitys;
 
+
+
 public class Note {
+
+private String _title;
+private String _content;
+
+public Note(String title)
+{
+    _title = title;
+    _content = "";
+
+}
+
+public Note (String title, String content)
+{
+    _title = title;
+    _content = content;
+}
+
+public String getTitel()
+{
+    return _title;
+}
+
+public String getContent()
+{
+    return _content;
+}
+
 
 }
